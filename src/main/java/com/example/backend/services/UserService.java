e028276487a85fa39338dc4ebbd26712dec8ef0b
Harrison25000 <ofcharrison@gmail.com>
2023-10-09 12:26:32 +0100

    login endpoint and logout endpoint added

--- src/main/java/com/example/backend/services/UserService.java
+++ src/main/java/com/example/backend/services/UserService.java
@@ -2,13 +2,16 @@ package com.example.backend.services;
 
 import com.example.backend.UserRepo;
 import com.example.backend.entities.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -66,6 +69,60 @@ public class UserService {
         }
         return hexString.toString();
 
+    }
+
+    public HashMap<String, Boolean> userLogin(User user, HttpSession session) throws NoSuchAlgorithmException {
+
+        // TODO: Refactor Please.
+
+        HashMap<String, Boolean> returnValue = new HashMap<>();
+
+        try {
+
+            if (user.getUserName().isEmpty() || user.getPassword().isEmpty()) {
+                returnValue.put("userLoggedIn", false);
+                return returnValue;
+            }
+
+            String password = user.getPassword();
+            String hashedPassword = toHexString(getSHA("teamGreen" + password));
+
+            try {
+                User existingUser = repo.findUserByUserName(user.getUserName());
+                if (Objects.equals(user.getUserName(), existingUser.getUserName()) && hashedPassword.equals(existingUser.getPassword())) {
+                    returnValue.put("userLoggedIn", true);
+                    session.setAttribute("id", existingUser.getId());
+                    session.setAttribute("isLoggedIn", true);
+                } else {
+                    returnValue.put("userLoggedIn", false);
+                }
+            } catch (Exception e) {
+                returnValue.put("userLoggedIn", false);
+                return returnValue;
+            }
+
+
+            return returnValue;
+
+        } catch(Exception e) {
+            // userName or Password is null
+            returnValue.put("userLoggedIn", false);
+            return returnValue;
+        }
+    }
+
+    public boolean userLogout(HttpSession session){
+
+//        RedirectView redirectView = new RedirectView();
+//        redirectView.setUrl("http://www.yahoo.com");
+//        return redirectView;
+
+        try {
+            session.invalidate();
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
     }
 
     public User findUserByUserName(String username){
