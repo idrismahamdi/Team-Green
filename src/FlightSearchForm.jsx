2932da88bbffe0af5c020593d1c2d97df3fcaf95
areebkhan1 <ak1657201@gmail.com>
2023-10-11 12:07:18 +0100

    updated flightsearchfrom with flight class, max price

--- src/FlightSearchForm.jsx
+++ src/FlightSearchForm.jsx
@@ -13,7 +13,8 @@ const FlightSearchForm = () => {
     const [passengers, setPassengers] = useState(1);
     const [fromSuggestions, setFromSuggestions] = useState([]);
     const [toSuggestions, setToSuggestions] = useState([]);
-    
+    const [flightClass, setFlightClass] = useState('ECONOMY');
+    const [maxPrice, setMaxPrice] = useState('');
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(airports);
@@ -42,9 +43,9 @@ const FlightSearchForm = () => {
 
     const handleBlur = (type) => {
         setTimeout(() => {
-            if(type === 'from') {
+            if (type === 'from') {
                 setFromSuggestions([]);
-            } else if(type === 'to') {
+            } else if (type === 'to') {
                 setToSuggestions([]);
             }
         }, 150); //delay
@@ -54,17 +55,17 @@ const FlightSearchForm = () => {
         if (query.length > 0) {
             const suggestions = airportData
                 .filter(airport => airport.airport.toLowerCase().includes(query.toLowerCase()))
-                .slice(0, 5); 
-            
-            if(type === 'from') {
+                .slice(0, 5);
+
+            if (type === 'from') {
                 setFromSuggestions(suggestions);
-            } else if(type === 'to') {
+            } else if (type === 'to') {
                 setToSuggestions(suggestions);
             }
         } else {
-            if(type === 'from') {
+            if (type === 'from') {
                 setFromSuggestions([]);
-            } else if(type === 'to') {
+            } else if (type === 'to') {
                 setToSuggestions([]);
             }
         }
@@ -74,15 +75,18 @@ const FlightSearchForm = () => {
         e.preventDefault();
         const fromAirportData = airportData.find(a => a.iata === fromAirport || a.airport === fromAirport);
         const toAirportData = airportData.find(a => a.iata === toAirport || a.airport === toAirport);
-        
+
         const dataToSend = {
             ...fromAirportData,
             ...toAirportData,
             departure: departureDate,
-            arrival: arrivalDate, 
+            arrival: arrivalDate,
             passengers: passengers,
+            flightClass: flightClass,
+            maxPrice: parseFloat(maxPrice) || null,
         };
-        
+
+
         try {
             await axios.post('your-api-endpoint', dataToSend);
             // Handle success
@@ -93,19 +97,19 @@ const FlightSearchForm = () => {
 
     return (
         <form className="flight-search-form" onSubmit={handleFormSubmit}>
-        <div className="input-wrapper">
-            <label htmlFor="fromAirport">From:</label>
-            <input
-                type="text"
-                id="fromAirport"
-                value={fromAirport}
-                onChange={(e) => {
-                    setFromAirport(e.target.value);
-                    suggest(e.target.value, 'from');
-                }}
-                onBlur={() => handleBlur('from')}
-                required
-            />
+            <div className="input-wrapper">
+                <label htmlFor="fromAirport">From:</label>
+                <input
+                    type="text"
+                    id="fromAirport"
+                    value={fromAirport}
+                    onChange={(e) => {
+                        setFromAirport(e.target.value);
+                        suggest(e.target.value, 'from');
+                    }}
+                    onBlur={() => handleBlur('from')}
+                    required
+                />
                 {fromSuggestions && (
                     <ul className="suggestions">
                         {fromSuggestions.map((suggestion, idx) => (
@@ -143,16 +147,38 @@ const FlightSearchForm = () => {
 
             <label htmlFor="departure-date">Departure Date:</label>
             <input type="date" id="departure-date" value={departureDate} onChange={(e) => setDeparture(e.target.value)} required />
-            
+
             <label htmlFor="arrival-date">Arrival Date:</label>
             <input type="date" id="arrival-date" value={arrivalDate} onChange={(e) => setArrival(e.target.value)} required />
-            
+
             <label htmlFor="passengers">Passengers:</label>
             <div className="passenger-input">
                 <button type="button" onClick={() => setPassengers(prev => Math.max(prev - 1, 1))}>-</button>
                 <input type="number" id="passengers" min="1" value={passengers} onChange={(e) => setPassengers(Math.max(Number(e.target.value), 1))} required />
                 <button type="button" onClick={() => setPassengers(prev => prev + 1)}>+</button>
             </div>
+
+            <label htmlFor="flightClass">Flight Class:</label>
+            <select
+                id="flightClass"
+                value={flightClass}
+                onChange={(e) => setFlightClass(e.target.value)}
+                required>
+                <option value="ECONOMY">Economy</option>
+                <option value="PREMIUM_ECONOMY">Premium Economy</option>
+                <option value="BUSINESS">Business</option>
+                <option value="FIRST">First</option>
+            </select>
+
+            <label htmlFor="maxPrice">Maximum Price:</label>
+            <input
+                type="number"
+                id="maxPrice"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+                placeholder="Enter max price"
+                step="5.00"
+            />
 
             <button type="submit">Search</button>
         </form>
