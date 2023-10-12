db99af7e6047871b27a5ac569ec47a64b0c82094
c-nim <c.nim.dev@gmail.com>
2023-10-12 12:54:23 +0100

    created a 'delete by user name' method

--- src/main/java/com/example/backend/UserRepo.java
+++ src/main/java/com/example/backend/UserRepo.java
@@ -1,6 +1,7 @@
 package com.example.backend;
 
 import com.example.backend.entities.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepo extends JpaRepository <User, Integer> {
 
     public User findUserByUserName(String username);
-
+    @Transactional
     public void deleteUserByUserName(String username);
 }
